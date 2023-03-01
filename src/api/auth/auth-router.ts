619a50a05845d3f988d90909d9a16c96c793e999
luis <luisrym@gmail.com>
2023-03-01 21:34:26 +0100

    add validator to auth router

--- src/api/auth/auth-router.ts
+++ src/api/auth/auth-router.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { registerUserController } from './register-controller.js';
 import { loginUserController } from './login-controller.js';
+import { registerValidation } from './register-validation.js';
+import { validate } from 'express-validation';
 
 const router = express.Router();
 
-router.route('/register').post(registerUserController);
+router
+  .route('/register')
+  .post(validate(registerValidation), registerUserController);
 router.route('/login').post(loginUserController);
 
 export default router;
