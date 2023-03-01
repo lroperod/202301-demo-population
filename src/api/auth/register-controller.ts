4f3c0e9c1e41ac069fa41c4ec06a4331e4b1f5b6
luis <luisrym@gmail.com>
2023-03-01 21:32:49 +0100

    remove error handling from controller

--- src/api/auth/register-controller.ts
+++ src/api/auth/register-controller.ts
@@ -3,7 +3,7 @@ import { User, UserModel } from '../user/user-schema.js';
 import crypto from 'node:crypto';
 import { encryptPassword } from './auth-utils.js';
 
-const EMAIL_REGEX_VALIDATION = /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/i;
+// Const EMAIL_REGEX_VALIDATION = /^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$/i;
 
 export type AuthRequest = Pick<User, 'email' | 'password'>;
 
@@ -13,14 +13,6 @@ export const registerUserController: RequestHandler<
   AuthRequest
 > = async (req, res) => {
   const { email, password } = req.body;
-
-  if (!EMAIL_REGEX_VALIDATION.test(email)) {
-    return res.status(400).json({ msg: 'Email must be a valid email' });
-  }
-
-  if (!password) {
-    return res.status(400).json({ msg: 'User is already registered in app' });
-  }
 
   try {
     const existingDBUser = await UserModel.findOne({ email }).exec();
