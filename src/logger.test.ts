aa580238fe22c7011ca5043ae10c185fbe098de1
luis <luisrym@gmail.com>
2023-03-01 20:11:49 +0100

    test logger

---
+++ src/logger.test.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import bunyan from 'bunyan';
+
+test('Given a logger', () => {
+  bunyan.createLogger = jest.fn();
+
+  bunyan.createLogger({ name: 'DemoApp' });
+
+  expect(bunyan.createLogger).toHaveBeenCalledTimes(0);
+});
